9ef5b7eda112b2312f79bf060ab100a3cda6f257
MBersPro <83613088+MBersPro@users.noreply.github.com>
2021-08-24 21:03:07 +0300

    commit

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,10 @@ const ingredients = [
   'Зелень',
   'Приправы',
 ];
+ingredients.forEach(item =>{
+  const variable = document.createElement('li')
+  variable.textContent = item
+  console.log(variable)
+  
+})
+document.querySelector('ul#ingredients').append(variable)
